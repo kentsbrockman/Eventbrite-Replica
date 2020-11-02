500c943472330e394cd689fda6335e5cfd62f65c
Quentin PLAUD <qplaud@gmail.com>
2020-11-02 22:20:19 +0100

    revamp event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,20 @@ class Event < ApplicationRecord
     presence: true,
     date: { after: Proc.new { Date.today }, message: 'must be after today' },
     on: :create
-  validates :duration, presence: true, numericality: { greater_than: 0 }, if: (:duration % 5).zero?
-  validates :title, presence: true, length: { in: 5..140 }
-  validates :description, presence: true, length: { in: 20..1000 }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000 }
+  validates :duration,
+    presence: true,
+    numericality: true,
+    if: :multiple_of_five?
+  validates :title,
+    presence: true,
+    length: { in: 5..140 }
+  validates :description,
+    presence: true,
+    length: { in: 20..1000 }
+  validates :price,
+    presence: true,
+    numericality: true,
+    numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000 }
   validates :location, presence: true
   
   has_many :attendances
@@ -15,6 +25,8 @@ class Event < ApplicationRecord
 
   belongs_to :user, class_name: "Admin"
 
-  
+  def multiple_of_five?
+    errors.add(:duration, "Should be a multiple of 5.") unless duration % 5 == 0
+  end
 
 end
