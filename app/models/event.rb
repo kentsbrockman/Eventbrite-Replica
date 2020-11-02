807d9ddf40ce6754562b440e993ffdfc561acdfa
Quentin PLAUD <qplaud@gmail.com>
2020-11-02 20:29:17 +0100

    Welcome mailer OK

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,28 +1,11 @@
 class Event < ApplicationRecord
 
-  validates :start_date,
-    presence: true
-    #start_date > Time.now
-
-  validates :duration,
-    presence: true
-    #value > 0
-    #value%5 = 0
-
-  validates :title,
-    presence: true,
-    length: { in: 5..140 }
-
-  validates :description,
-    presence: true,
-    length: { in: 20..1000 }
-
-  validates :price,
-    presence: true
-    #value: { in: 1..1000 }
-
-  validates :location,
-    presence: true
+  validates :start_date, presence: true, if: :start_date.future?
+  validates :duration, presence: true, numericality: { greater_than: 0 }, if: (:duration % 5).zero?
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000 }
+  validates :location, presence: true
   
   has_many :attendances
   has_many :users, through: :attendances
