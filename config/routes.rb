5d531520f986dc8794b06e7c4221e85016983d3e
Quentin PLAUD <qplaud@gmail.com>
2020-11-03 17:03:53 +0100

    Navbar complete 1st try

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   root 'events#index'
 
-  get 'events/secret'
+  get 'events/index', to: 'events#home'
+  get 'events/secret', to: 'events#secret'
+  get 'events/team', to: 'events#team'
+  get 'events/contact', to: 'events#contact'
+  #get 'events/privacy', to: 'events#privacy'
+  #get 'events/terms', to: 'events#terms'
+
+
+
   devise_for :users
 
 end
