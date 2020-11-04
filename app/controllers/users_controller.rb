bbd1056287432e74ef26038e5ccd5031e268f9c7
Quentin PLAUD <qplaud@gmail.com>
2020-11-04 22:52:00 +0100

    Basic view user profile ok with edit options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,38 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!, only: [:show]
+  before_action :check_user, only: [:show]
+
   def show
+    @user = current_user
+    @events = Event.where(admin: @user)
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User updated in DB"
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:alert] = "We cannot updated this gossip for this reason(s) :"
+      render :edit
+    end
+  end
+
+  def check_user
+    @user = User.find(params[:id])
+    unless current_user.id == @user.id
+       redirect_to root_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description)
   end
   
 end
